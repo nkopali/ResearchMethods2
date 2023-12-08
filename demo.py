1e5fc5f1ba1304b35d07b58916746e0e728447a3
Nils <nils.kopali20@live.com>
2023-12-08 15:20:52 +0100

    adding demo

--- demo.py
+++ demo.py
@@ -8,7 +8,7 @@ from ultralytics import YOLO
 def main():
     st.title("Image Upload and Model Prediction")
 
-    model = YOLO('yolov8n-cls.pt')  
+    model = YOLO('runs/classify/train/weights/best.pt')  
 
     # File uploader 
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
@@ -16,12 +16,12 @@ def main():
         image = Image.open(io.BytesIO(uploaded_file.read()))
         
         # Display the image
-        st.image(image, caption='Uploaded Image', use_column_width=True)
+        st.image(image, caption='Uploaded Image', width=300)
 
         # Make a prediction
         if st.button('Predict'):
-            result = model.predict(image)
-            st.write(result)
+            result = model(image)
+            st.write(model.names[result[0].probs.top1])
 
 if __name__ == '__main__':
     main()
